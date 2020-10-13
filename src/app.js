9ab4f00fc918ef70099d956287b991c4e6f0e71f
Marc Robotham <forxnives@gmail.com>
2020-10-12 20:20:59 -0700

    adding playlist functionality

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import KnobCreate from './KnobCreate/KnobCreate.js';
 
 import Deck from './Deck/Deck.js';
 import PlayList from './PlayList/PlayList.js';
+import State from './State/State.js';
 
 
 // SCKEY1 = 'a3dd183a357fcff9a6943c0d65664087';
@@ -17,12 +18,14 @@ document.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
 
-
+  const state = new State();
+  
+  
   const deck1 = new Deck('1');
 
   const deck2 = new Deck('2');
 
-  const playlist = new PlayList();
+  const playlist = new PlayList(deck2, state);
 
   
 
